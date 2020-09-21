c152f6c7c6fa64b27eba69fd99639903f53534b0
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-21 03:03:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(string)
+   string.select do |element|
+        element.split("").sort == word.split("").sort   #splits into letters and then sorts alphabetically
+      end
+  end
+  
+end
